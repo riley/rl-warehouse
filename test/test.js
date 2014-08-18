baf012d38479b36149facedad5493bc1c7d93e6d
Riley Davis <riley.davis@gmail.com>
2014-08-18 17:55:57 -0400

    update with tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,10 @@
+var config = require('./config.json');
+
+process.env.RL_USER = config.username;
+process.env.RL_PASSWORD = config.password;
+
 var warehouse = require('../lib/warehouse');
 
-warehouse.findOrder('53b75VEX', function (err, order) {
-    console.log(order);
+warehouse.findOrder('53f20a697140dbb565000001', function (err, order) {
     process.exit();
 });
