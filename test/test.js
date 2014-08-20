dd8b301f4a2c50d81270224526cae5b14ed86a56
Riley Davis <riley.davis@gmail.com>
2014-08-19 21:11:44 -0400

    checking for programmer errors

--- test/test.js
+++ test/test.js
@@ -5,12 +5,13 @@ process.env.RL_PASSWORD = config.password;
 
 var warehouse = require('../lib/warehouse');
 
-// warehouse.findOrder('53f20a697140dbb565000001', function (err, order) {
-//     process.exit();
-// });
-
-warehouse.getInventory(function (err, inv) {
-    if (err) console.log(err);
-    console.log(JSON.stringify(inv, null, 2));
+warehouse.findOrder('53f35be8d389a9436f000002', function (err, order) {
+    console.log(JSON.stringify(order, null, 2));
     process.exit();
 });
+
+// warehouse.getInventory(function (err, inv) {
+//     if (err) console.log(err);
+//     console.log(JSON.stringify(inv, null, 2));
+//     process.exit();
+// });
