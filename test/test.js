4ae86d23da6ecba1a4f31177a3c721f802215515
Riley Davis <riley.davis@gmail.com>
2014-08-26 20:14:36 -0400

    better reporting for order not found

--- test/test.js
+++ test/test.js
@@ -5,9 +5,15 @@ process.env.RL_PASSWORD = config.password;
 
 var warehouse = require('../lib/warehouse');
 
-warehouse.findOrder('53f35be8d389a9436f000002', function (err, order) {
-    console.log(JSON.stringify(order, null, 2));
+warehouse.findOrder('ref_hzakgyvg', function (err, order) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(JSON.stringify(order, null, 2));
+    }
     process.exit();
+
+
 });
 
 // warehouse.getInventory(function (err, inv) {
